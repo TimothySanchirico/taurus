1cd6200c912420a77ae89c7ba9b2db5efcd54bfd
mworley2 <mworley2@illinois.edu>
2015-09-19 15:31:00 -0500

    taurus.js modifications

--- taurus.js
+++ taurus.js
@@ -13,18 +13,16 @@ Router.route('/map', {
 });
 
 //setup mongoDBs
-tourist_collection = new Mongo.Collection('tourists');
-guide_collection = new Mongo.Collection('guides');
+tourist_collection = new Mongo.Collection("tourists");
+guide_collection = new Mongo.Collection("guides");
 
 
 if (Meteor.isClient) {
+  console.log("Debug Line 21")
   // counter starts at 0
   Meteor.subscribe('tourists');
   Meteor.subscribe('guides');
   
-
-
-
   Template.tourguideSignUp.events({
     'submit .guide_form': function (event) {
       event.preventDefault();
@@ -41,7 +39,7 @@ if (Meteor.isClient) {
       //alert(locations + " " + interests);
       var touristguide_locations_array = locations.split(",");
       var touristguide_interests_array = interests.split(",");
-      guide_db.insert({
+      guide_collection.insert({
           name: { first: event.target.guide_first.value, last: event.target.guide_last.value },
           touristguide_locations: touristguide_locations_array,
           touristguide_interests_array: touristguide_interests_array,
@@ -52,13 +50,13 @@ if (Meteor.isClient) {
       event.target.guide_last.value = "";
       event.target.guide_dest.value = "";
       event.target.guide_interest.value = "";
-      
+
     }
   });
 
   Template.touristSignUp.events({
     'submit .tourist_form': function (event) {
-      
+      event.preventDefault();
 
       //grab the form data
       var name = event.target.tourist_first.value + ' ' + event.target.tourist_last.value;
@@ -69,7 +67,7 @@ if (Meteor.isClient) {
       var tourist_locations_array = locations.split(",");
       var tourist_interests_array = interests.split(",");
       //push these into the tourist database
-      tourist_db.insert({
+      tourist_collection.insert({
           name: { first: event.target.tourist_first.value, last: event.target.tourist_last.value },
           tourist_locations: tourist_locations_array,
           tourist_interests_array: tourist_interests_array,
@@ -80,6 +78,8 @@ if (Meteor.isClient) {
       event.target.tourist_last.value = "";
       event.target.tourist_dest.value = "";
       event.target.tourist_interest.value = "";
+
+      Session.set('name', name);
 
     }
   });
