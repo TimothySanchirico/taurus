4a2a6231bdc53d529b5930a1ac03dacdc13d1721
Radhir Kothuri <kothuri2@illinois.edu>
2015-09-20 04:36:18 -0500

    dynamic matches rendering

--- matches.js
+++ matches.js
@@ -2,11 +2,9 @@
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.matches.helpers({
-    tasks: [
-      { text: "google.com" },
-      { text: "dongle"},
-      { text: "dang"}
-    ]
+    tasks: function () {
+      return guide_collection.find({});
+    }
   });
 }
 
