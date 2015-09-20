e63a515028b46dd4f53fc24da18ba40e658db205
mworley2 <mworley2@illinois.edu>
2015-09-20 04:53:28 -0500

    javascript

--- matches.js
+++ matches.js
@@ -4,7 +4,30 @@ if (Meteor.isClient) {
   Template.matches.helpers({
     tasks: function () {
       return guide_collection.find({});
+      tourGuideScoreCalc(Session.get('this_session'));
+      if(Session.get('type') == "tourist") {
+      	guideScoreCalc(Session.get('this_session'));
+      }
+      else
+      {
+      	touristScoreCalc(Session.get('this_session'));
+      }
     }
   });
 }
 
+function guideScoreCalc(sessionval) {
+  var touristCursor = tourist_collection.find({});
+  console.log(touristCursor);
+  var tourist;
+  var guide = guide_collection.find({_id: sessionval}).fetch();
+  var orderedOutput = new Array(0);
+  while(touristCursor.hasNext()) {
+    tourist = touristCursor.next().fetch();
+ 
+  }
+}
+
+function touristScoreCalc(sessionval) {
+	
+}
